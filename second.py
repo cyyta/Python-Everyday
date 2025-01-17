cb3f7991a0c3960cd52665a6c33249e52bccb0a2
Cyyta <be.cyyta@gmail.com>
2025-01-17 17:27:09 +0545

    changing variable process

--- second.py
+++ second.py
@@ -2,7 +2,6 @@
 #create variables,print variables,changed the values assigned to the variables
 #here,greeting is a variable and Happy Tihar is a value assigned to greeting
 #for displaying output use print() function
-
 greeting = "Happy Tihar"
 print(greeting)  #it display the value
 print("greeting") #it display greeting
