f1a0acb4aaa88cda4b20702d403f989208c644ac
Cyyta <be.cyyta@gmail.com>
2025-01-17 18:49:36 +0545

    variables creation

--- third.py
+++ third.py
@@ -1,6 +1,10 @@
 #change values stored in a variable
 #the value of variable can change as the program runs,which is way it is called a variable
-age=23  #age is 23
-print("age is:",age)  #23 is printed
-age=24    #age is 25
-print("age is:",age)  #24 is printed
+#age=23  #age is 23
+#print("age is:",age)  #23 is printed
+#age=24    #age is 25
+#print("age is:",age)  #24 is printed 
+
+age = 21
+age += 1
+print("My age is:"+str(age))
