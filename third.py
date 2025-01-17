bf5e6b253d2844f9ca16b44910e5e8eb90be21b6
Cyyta <be.cyyta@gmail.com>
2025-01-17 17:29:35 +0545

    changing variable process

---
+++ third.py
@@ -0,0 +1,6 @@
+#change values stored in a variable
+#the value of variable can change as the program runs,which is way it is called a variable
+age=23  #age is 23
+print("age is:",age)  #23 is printed
+age=24    #age is 25
+print("age is:",age)  #24 is printed
