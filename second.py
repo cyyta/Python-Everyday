c84e3501644457ffddf240b2a3b10f5351778be6
Cyyta <be.cyyta@gmail.com>
2025-01-17 17:08:30 +0545

    variables created

---
+++ second.py
@@ -0,0 +1,9 @@
+#Variable: variable is a container for value(string,integer,float,boolean)
+#create variables,print variables,changed the values assigned to the variables
+#here,greeting is a variable and Happy Tihar is a value assigned to greeting
+#for displaying output use print() function
+
+greeting = "Happy Tihar"
+print(greeting)  #it display the value
+print("greeting") #it display greeting
+
